686b908d7ab49b5d5333bf23db08c547c3435f6d
Gabriel Esayas <gesayas@mit.edu>
2017-07-18 22:08:20 -0500

    updated documentation and removed WeightFinder code to only showcase playing a game and running Simulators

--- RunSims.java
+++ RunSims.java
@@ -3,25 +3,22 @@ package project2;
 class RunSims {
 	
 	/*
-	 * This class is used to create instances of Simulator objects in order to run games
-	 * and Simulations of Battleship.
+	 * This class is used to play a game of BattleShip and to create instances
+	 * of Simulator objects in order to run simulations of Battleship.
 	 */
 	
 	public static void main(String[] args) {
-//		BattleShip bs = new BattleShip(true);
-//		bs.playGame();
+		BattleShip bs = new BattleShip(true);
+		bs.playGame(); // plays a game between a human and computer
 		
-//		BattleSimulator bsim = new BattleSimulator(10000, false, 3, 4);
-//		bsim.runSim();
+		BattleSimulator bsim = new BattleSimulator(10000, false, 3, 4);
+		bsim.runSim(); // plays a game between two computers
 		
-//		TestSimulator ts1 = new TestSimulator(1, 4, true);
-//		ts1.runSim();
-//		TestSimulator ts2 = new TestSimulator(100000, 4, false);
-//		ts2.runSim();
-//		TestSimulator ts3 = new TestSimulator(1000, 4, false);
-//		ts3.runSim();
-		
-//		WeightFinder wf = new WeightFinder(100000);
-//		wf.run();
+		TestSimulator ts1 = new TestSimulator(1, 4, true);
+		ts1.runSim(); // tests the level 4 strategy one time with print statements
+		TestSimulator ts2 = new TestSimulator(100000, 4, false);
+		ts2.runSim(); // tests the level 4 strategy with 100000 boards, no print statements
+		TestSimulator ts3 = new TestSimulator(1000, 4, false);
+		ts3.runSim();
 	}
 }
